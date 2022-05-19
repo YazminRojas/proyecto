3362c95e987d1eeb803b861c963b44448584e5d8
Almita05 <martinezlanderoalmalil@gmail.com>
2022-05-19 08:32:56 -0500

    Merge branch 'master' of https://github.com/YazminRojas/proyecto

--- routes/web.php
+++ routes/web.php
@@ -6,11 +6,7 @@ use App\Http\Controllers\EdificioController;
 
 
 Route::resources([
-<<<<<<< HEAD
     'Edificios'       => EdificioController::class,
-=======
-    'agenda'=> AgendaController::class,
->>>>>>> 5a320abd48ba385d1f9bc8f0d2d3ab51d6c44253
 ]);
 
 Route::get('/', function () {
@@ -28,3 +24,7 @@ Route::get('/', function () {
 });
 
 //-------------------------------------------------------------------
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
