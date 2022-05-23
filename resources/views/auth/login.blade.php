8bd8e683bb36dc1d409c9f9a47876750cd368ff6
Almita05 <martinezlanderoalmalil@gmail.com>
2022-05-23 11:28:47 -0500

    cambios de login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -65,10 +65,12 @@
 
                         <div class="row mb-0">
                             <div class="col-md-8 offset-md-4">
-                            <button type="button" class="btn btn-success">Ingresar</button>
+                            <button type="button" class="btn btn-success" href="{{url('materias/')}}">Ingresar</button>
                                     
                                 </button>
 
+
+                                
                                 @if (Route::has('password.request'))
                                     <a class="btn btn-link" href="{{ route('password.request') }}">
                                         {{ __('¿Olvidaste tu contraseña?') }}
