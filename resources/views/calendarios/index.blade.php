1560e94c62ed0c7f35d3dd6c6cda5eca206fed80
isverse31 <darioreyea974@gmail.com>
2022-05-25 10:41:16 -0500

    holi

--- resources/views/calendarios/index.blade.php
+++ resources/views/calendarios/index.blade.php
@@ -1,9 +1,6 @@
 @extends('layout.layout')
 @section('content')
-
 <center><article class="panel is-success" >
 <h1 class="panel-heading">AGENDA ADAM</h1></center>
-
-
 
 <center><img src="https://png.pngtree.com/png-vector/20210713/ourlarge/pngtree-may-2022-calendar-png-image_3573089.jpg" width="600px" heigth="600px"></center><br>
