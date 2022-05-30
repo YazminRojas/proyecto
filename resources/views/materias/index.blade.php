29cdf99ad583100806e07d936ebb865e66ce0c93
Almita05 <martinezlanderoalmalil@gmail.com>
2022-05-30 12:06:36 -0500

    hola Yaz, estos son mis neuvos cambios

--- resources/views/materias/index.blade.php
+++ resources/views/materias/index.blade.php
@@ -6,8 +6,7 @@
     <article class="panel is-info">
         <h1 class="panel-heading">AGENDA ADAM</h1>
 </center>
-
-<article class="panel is-orange">
+<article class="panel is-success">
     <p class="panel-heading">
         Materia
     <div class="tabs is-right  is-toggle is-toggle-rounded">
@@ -27,20 +26,20 @@
         <center>
             <a class="button is-danger is-light" href="/materias/create">+ Materia</a>
         </center>
-        <br><br><br>
         <div>
 
             <section class="section">
                 <div class="table-container">
-                    <table class="table is-bordered is-striped is-narrow is-hoverable is-fullwidth" bgcolor=#990000>
+                    <table class="table is-bordered is-striped is-narrow is-hoverable is-fullwidth">
                         <thead>
-                            <tr>
-                                <th>id</th>
-                                <th>Nombre de la materia</th>
-                                <th>Clave</th>
-                                <th>Docente</th>
-                                <th>Edificio</th>
-                                <th>Salon</th>
+                            <tr class="is-size-4" font-size="1.5rem" class="has-text-weight-semibold">
+                                <th class="has-text-centered">Id</th>
+                                <th class="has-text-centered">Nombre de la materia</th>
+                                <th class="has-text-centered">Clave</th>
+                                <th class="has-text-centered">Docente</th>
+                                <th class="has-text-centered">Edificio</th>
+                                <th class="has-text-centered">Salón</th>
+                                <th class="has-text-centered">Acciones</th>
                             </tr>
                         <tbody>
 
@@ -56,12 +55,12 @@
 
                                 <td>
                                     <form action="{{ route('materias.destroy', $materia->id) }}" method="POST">
-                                        <a class="button is-info is-mall"
+                                        <a class="button is-warning"
                                             href="{{route('materias.edit' , $materia->id)}}">Editar</a>
                                         <a href="{{url('compartirs/')}}" class="button is-info is-mall">Compartir</a>
                                         @csrf
                                         @method('DELETE')
-                                        <button type="submit" class="button is-primary"> eliminar </button>
+                                        <button type="submit" class="button is-danger"> Eliminar </button>
                                     </form>
                                 </td>
                             </tr>
