fbeebb91719b97c94fcec9bf857ee00be40933ed
YazminRojas <yazminrojas926@gmail.com>
2022-05-30 11:03:08 -0500

    cambios nuevos

--- resources/views/perfils/create.blade.php
+++ resources/views/perfils/create.blade.php
@@ -1,69 +1,26 @@
 @extends('layout.layout')
 @section('content')
 
-
-<center><article class="panel is-success" >
-<h1 class="panel-heading">Agenda ADAM</h1></center>
-
-<article class="panel is-warning" >
     
-  <p class="panel-heading">
-    Perfil
-    
-  </p>
-    </section>
 
-    <html>
-        <body bgcolor="pink">
-        <center>
-        <br><br><br>
-    <div>
-
-    <form action="{{ route('perfils.store') }}"  method="post" >
-            @csrf
-            <fieldset>
-                
-            <div class="form-group">
-                <label for="">Nombre del perfil:</label>
-                <input type="text" name="NombrePerfil" id="" size="40">
+            <div class="row">
+        <div class="col-12">
+        <form action="{{ route('perfils.store') }}"  method="post" >
+            
+                @csrf
+                <div class="form-group">
+                    <label class="label">Nombre</label>
+                    <input required autocomplete="off" name="nombre" class="form-control"
+                           type="text" placeholder="Nombre">
                 </div>
-            </div>
-<br>
-            <div class="form-group">
-                <label for="">Nombre:</label>
-                <input type="text" name="NombrePersona" id="" size="37">
-            </div>
-<br>
-            <div class="form-group">
-                <label for="">Apellido Paterno:</label>
-                <input type="text" name="ApellidoPaterno" id="" size="30">
-            </div>
 
-            <br>
-            <div class="form-group">
-                <label for="">Apellido Materno:</label>
-                <input type="text" name="ApellidoMaterno" id="" size="30">
-            </div>
-
-            <br>
-            <div class="form-group">
-                <label for="">Número de teléfono:</label>
-                <input type="text" name="NumeroTelefono" id="" size="30">
-            </div>
-
-            <br>
-            <div class="form-group">
-                <label for="">Descripción:</label>
-                <textarea name="Descripcion" id="" cols="30" rows="5" placeholder="Escriba una descripción..."></textarea>
-            </div>
-
-<br> <br>
-            <div class="mt-5">
-                <input class="btn btn-primary" type="submit" value="Guardar"
-                <a class="button is-success" href="{{url('perfils/')}}">Cancelar</a>
-            </div>
-    </form>
-        </body>
-    </html>
-   
+                @include('modal')
+                <button class="btn btn-success">Guardar</button>
+                <a class="btn btn-primary" href="{{route(perfils.index')}}">Volver al listado</a>
+            </form>
+        </div>
+    </div>
+            
+            
+    
 @endsection
